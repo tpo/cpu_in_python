9b44a46191ff12ecbe89034c58d30cd4bddf368f
Tomas Pospisek <tpo@sourcepole.ch>
2021-09-04 09:41:21 +0200

    layout/whitespace/prettify

--- cpu.py
+++ cpu.py
@@ -69,10 +69,10 @@ class Cpu:
   def dump_cpu_state(self):
     print("CPU state")
     print("---------")
-    print("ACC:    %d" % self.acc)
-    print("OP:     %d" % self.operation)
-    print("OP_ARG: %d" % self.operation_argument)
-    print("PC:     %d" % self.pc)
+    print("ACC:     %d" % self.acc)
+    print("OP:      %d" % self.operation)
+    print("OP_ARG:  %d" % self.operation_argument)
+    print("PC:      %d" % self.pc)
     print("mem[%d]: %d" % (self.debug_watch_addr, self.memory[self.debug_watch_addr]))
 
 cpu = Cpu()
